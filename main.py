75c084ae2182d5b6b347c5b78d5e4cd2a478a3e6
5OR627 <97848016+5OR627@users.noreply.github.com>
2022-01-27 20:20:29 +0300

    Дз 4

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ print([el for el in gen])
 """Задача 5"""
 
 
-my_list = [i for i in range(100, 1001) if i % 2 == 0]
+my_list = [i for i in range(100, 1001, 2)]
 print(reduce(lambda x, y: x * y, my_list))
 
 
